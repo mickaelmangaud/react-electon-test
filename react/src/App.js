4e33848f01d44e7703b94992de48f38402bcae5f
Mickael Mangaud <mickael.mangaud@vitalliance.fr>
2021-09-15 15:20:18 +0200

    ménage

--- react/src/App.js
+++ react/src/App.js
@@ -1,18 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import appRuntime from './appRuntime';
-
-console.log(appRuntime);
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Electron React</p>
-      </header>
-    </div>
-  );
+  return <div>React Electron</div>;
 }
 
 export default App;
